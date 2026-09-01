ed70cfb3002eecc4b83398f5fe6c767cff184c55
Gui <guilhermerafaell10@gmail.com>
2026-08-31 21:36:36 -0300

    exemplo de agregação Conta efetuado

---
+++ AgregacaoConta/Program.cs
@@ -0,0 +1,12 @@
+﻿using AgregacaoConta;
+
+
+Cliente cli = new Cliente("Gui", 123);
+Conta c1 = new Conta(1, 150);
+// EFETIVAÇÃO DA AGREGAÇÃO
+c1.Titular = cli;
+c1.Mostrar();
+
+Conta c2 = new Conta(2, 200);
+c2.Titular = new Cliente("Jão", 422);
+c2.Mostrar();
